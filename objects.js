bb8efea8794f90fa5624190892c15708dc461e47
Dean14692-prog <dennismuasyangui@gmail.com>
2025-03-11 17:14:20 +0300

    Second Commit

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
- /*
+/*
  Objects are collection of related properties or methods
  An object literal is a list of name:value pairs inside curly braces {}.
         name:value pairs are also called key:value pairs.
@@ -6,10 +6,10 @@
  */
 
 const person = {
-    firstName: "Dennis",
-    secondName: "Ngui",
-    age : 30
-}
+  firstName: "Dennis",
+  secondName: "Ngui",
+  age: 30,
+};
 
 /*
 To print values of the object we use this syntax
@@ -18,7 +18,6 @@ For my case the object name is person.
 */
 
 console.log(person.age);
-Object.getOwnPropertyNames(person);
 
 /*
 To list the properties of the object we use
@@ -39,4 +38,16 @@ To print the values we use the following syntax
  */
 console.log(Object.values(person));
 
-
+/*
+To make changes on the object or to add new properties on the object we use
+---> object.property = value;
+Let say i want to include Height property with a Tall value
+my command will be
+---> "Object Name"."Property/Key to be added on the Object" = " Value of the Key"
+*/
+person.Height = "Tall"; // Adding Height Key with Tall value
+console.log(person); // To print the object values and keys
+console.log(Object.values(person)); // To Print Values of the
+console.log(Object.keys(person)); // To print Keys of the Object
+person.Cars = 3; // Adding Cars in the Object
+console.log(person);
